236bdc4f9cd77afaba7a1774c86931fe7621e254
Bmercado17 <brnrdsnchz17@gmail.com>
2020-08-18 05:24:00 -0700

    ticket price and selection

--- scripts/myscripts.js
+++ scripts/myscripts.js
@@ -5,20 +5,34 @@ let count = document.getElementById('count');
 let total = document.getElementById('total');
 let movieSelect = document.getElementById('movie');
 
-// targetting and grabbing the value 
+// targetting and grabbing the value per ticket
 let ticketPrice = +movieSelect.value;
 
-//Functions
+//Functions section
 //Functions to Update total and Count
 function updateSelectedCount() {
-    let selectedSeats = document.querySelectorAll('.row.seat.selected');
-    console.log(selectedSeats)
+    let selectedSeats = document.querySelectorAll('.row .seat.selected');
+    // pay atte, the spacing above insde the parenthesis ,row should b stand alne
+    let selectedSeatsCount = selectedSeats.length;
+
+
+    count.innerText = selectedSeatsCount; //innerText changes either text or number
+    total.innerText = selectedSeatsCount * ticketPrice;
 }
-// First event listener
-container.addEventListener('click', (e) => {
+// event listeners section
+// event listener Movie Select
+movieSelect.addEventListener('change', e => {
+    ticketPrice = +e.target.value;
+
+})
+
+//  event listener Seat Select
+
+container.addEventListener('click', e => {
     if (e.target.classList.contains('seat') &&
         !e.target.classList.contains('occupied')
     ) {
         e.target.classList.toggle('selected');
+        updateSelectedCount();
     }
 })
